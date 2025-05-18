443efe766782abe880bea94b9751192eb83e6565
Muhammad-Bilal1605 <zaheermbilal1605@gmail.com>
2025-05-19 00:53:13 +0500

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -45,10 +45,11 @@ const startServer = async () => {
   try {
     connectDB();
     const port = process.env.PORT || 4000;
-    app.listen(port, () => console.log(`Server started on port ${port}`));
+    app.listen(port, () => console.log(`Servers started on port ${port}`));
   } catch (error) {
     console.log(error);
   }
 };
 
 startServer();
+//hello
